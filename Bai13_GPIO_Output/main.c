68e0fe1c4b3d9f4973eb17f2a3086fa8a9089854
LAPTOP-A76MUA6E\Linh <lequanglinh1412@gmail.com>
2021-07-16 01:20:26 +0700

    no message

--- Bai13_GPIO_Output/main.c
+++ Bai13_GPIO_Output/main.c
@@ -1,14 +1,13 @@
 #include "stm32f10x.h"
 
 void Delay_Ms(uint32_t u32Delay);
+void GPIO_Register_Config();
+void GPIO_Lib_Config();
+void GPIO_Config_Lib();
 
 int main()
 {
-	RCC->APB2ENR |= 1<<3;
-	RCC->APB2ENR |= 1<<4;
-	/*GPIOC[15:8] output Pushpull*/
-	GPIOC->CRH = 0x11111111;
-
+	GPIO_Lib_Config();
 	
 	while(1)
 	{
@@ -19,12 +18,32 @@ int main()
 		/*GPIO_TypeDef *ptr = 0x40000000 + 0x10000 + 0x1000;
 		ptr->ODR = 0xffff;*/
 		
-		GPIOC->ODR = 0xffff
+		GPIOC->ODR = 0xffff;
 		Delay_Ms(1000);
 		GPIOC->ODR = 0x00;
 		Delay_Ms(1000);
 	}
 
+}
+
+void GPIO_Register_Config()
+{
+	RCC->APB2ENR |= 1<<3;
+	RCC->APB2ENR |= 1<<4;
+	/*GPIOC[15:8] output Pushpull*/
+	GPIOC->CRH = 0x11111111;
+}
+void GPIO_Lib_Config()
+{
+	GPIO_InitTypeDef GPIO_InitStrcture;
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	
+	GPIO_InitStrcture.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStrcture.GPIO_Pin = GPIO_Pin_13;
+	GPIO_InitStrcture.GPIO_Speed = GPIO_Speed_50MHz;
+	
+	GPIO_Init(GPIOC, &GPIO_InitStrcture);
 }
 
 void Delay_Ms(uint32_t u32Delay)
